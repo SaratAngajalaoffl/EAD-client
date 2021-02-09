c6c0d0043b6f51501c9dd1281d1ec9a58ce49737
SaratAngajalaoffl <saratangajala@gmail.com>
2021-02-09 17:22:48 +0530

    Initial Commit

---
+++ src/components/sections/Hero.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import classNames from 'classnames';
+import { SectionProps } from '../../utils/SectionProps';
+import ButtonGroup from '../elements/ButtonGroup';
+import Button from '../elements/Button';
+import Image from '../elements/Image';
+
+const propTypes = {
+	...SectionProps.types,
+};
+
+const defaultProps = {
+	...SectionProps.defaults,
+};
+
+const Hero = ({
+	className,
+	topOuterDivider,
+	bottomOuterDivider,
+	topDivider,
+	bottomDivider,
+	hasBgColor,
+	invertColor,
+	...props
+}) => {
+	const outerClasses = classNames(
+		'hero section center-content',
+		topOuterDivider && 'has-top-divider',
+		bottomOuterDivider && 'has-bottom-divider',
+		hasBgColor && 'has-bg-color',
+		invertColor && 'invert-color',
+		className
+	);
+
+	const innerClasses = classNames(
+		'hero-inner section-inner',
+		topDivider && 'has-top-divider',
+		bottomDivider && 'has-bottom-divider'
+	);
+
+	return (
+		<section {...props} className={outerClasses}>
+			<div className='container-sm'>
+				<div className={innerClasses}>
+					<div className='hero-content'>
+						<h1
+							className='mt-0 mb-16 reveal-from-bottom'
+							data-reveal-delay='200'>
+							A ONE STOP DESTINATION FOR HANDLING{' '}
+							<span className='text-color-primary'>
+								COURSE WORK
+							</span>
+						</h1>
+						<div className='container-xs'>
+							<p
+								className='m-0 mb-32 reveal-from-bottom'
+								data-reveal-delay='400'>
+								We are a solution for conducting exams during
+								this pandemic with the added flexibility of our
+								API for accessing statistics.
+							</p>
+							<div
+								className='reveal-from-bottom'
+								data-reveal-delay='600'>
+								<ButtonGroup>
+									<Button
+										tag='a'
+										color='primary'
+										wideMobile
+										href='https://cruip.com/'>
+										Student/Instructer Login
+									</Button>
+									<Button
+										tag='a'
+										color='dark'
+										wideMobile
+										href='https://github.com/cruip/open-react-template/'>
+										Institute Login
+									</Button>
+								</ButtonGroup>
+							</div>
+						</div>
+					</div>
+					<div
+						className='hero-figure reveal-from-bottom illustration-element-01'
+						data-reveal-value='20px'
+						data-reveal-delay='800'>
+						<Image
+							className='has-shadow'
+							src={require('./../../assets/images/video-placeholder.jpg')}
+							alt='Hero'
+							width={700}
+							height={500}
+						/>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+Hero.propTypes = propTypes;
+Hero.defaultProps = defaultProps;
+
+export default Hero;
