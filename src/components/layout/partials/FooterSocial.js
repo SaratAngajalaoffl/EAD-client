8b9c358d99f0231625318e91cf6a9d89477f3cf7
RaganMurali <53899110+RaganMurali@users.noreply.github.com>
2021-05-05 22:07:52 +0530

    instagram url updated

--- src/components/layout/partials/FooterSocial.js
+++ src/components/layout/partials/FooterSocial.js
@@ -44,7 +44,7 @@ const FooterSocial = ({
           </a>
         </li>
         <li>
-          <a href="https://google.com/">
+          <a href="https://www.instagram.com/">
             <svg
               width="16"
               height="16"
